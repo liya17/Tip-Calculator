e6da99319b46761d7f7ab394b85d1153e9503127
liya17 <17liyaw@gmail.com>
2016-06-22 13:42:23 -0400

    Initial Commit

---
+++ TipPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipPro
+//
+//  Created by Liya Wu-17 on 6/22/16.
+//  Copyright © 2016 ms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
